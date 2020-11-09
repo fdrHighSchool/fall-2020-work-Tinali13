ca838bf2a0def5b8dc530d7ae6004f4c9377b8b1
Tinali13 <tli7392@fdrhs.org>
2020-11-09 08:46:57 -0500

    finished checkpoint 2

--- 08- fraction calculator/FracCalc.java
+++ 08- fraction calculator/FracCalc.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class FracCalc {
+class Main {
   public static void main(String[] args) {
     Scanner scan = new Scanner (System.in);
     System.out.println("Enter your problem here: ");
@@ -15,6 +15,34 @@ public static String produceAnswer(String input){
   System.out.println("Op:" +operator);
   String fraction2 = input.substring(fraction1.length() + 3);
   System.out.println("Frac 2:" +fraction2);
-  return (fraction2);
+  //return (fraction2);
+  String Whole = findWhole(fraction2);
+  String Num = findNum(fraction2);
+  String Denom = findDenom(fraction2);
+  String checkpoint2 = "Whole:" + Whole + "numerator:" + Num + "denominator:" +Denom;
+  return checkpoint2;
  }
+ public static String findWhole(String str){
+   if (str.contains("_")){
+     return str.substring(0, str.indexOf('_'));
+   }else if (str.contains("/")){
+     return "0";
+  }else return str;
+ }
+  public static String findNum (String str){
+    if (str.contains("_")){
+      return str.substring(str.indexOf('_')+ 1, str.indexOf('/'));
+    }else if (str.contains("/")){
+      return str.substring(0,str.indexOf('/'));
+    }else{
+      return "0";
+    }
+  }
+  public static String findDenom(String str){
+    if (str.contains("/")){
+      return str.substring(str.indexOf("/")+1);
+    }else{
+      return "1";
+    }
+  }
 }
