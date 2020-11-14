48f41c9fdb41b4b75fd76012224ed5f94269749c
Tinali13 <tli7392@fdrhs.org>
2020-11-13 22:45:55 -0500

    finsihed checkpoint 2

--- 08- fraction calculator/FracCalc.java
+++ 08- fraction calculator/FracCalc.java
@@ -1,48 +1,62 @@
 import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
-    Scanner scan = new Scanner (System.in);
-    System.out.println("Enter your problem here: ");
-    String problem = scan.nextLine();
-    System.out.println(problem);
-    produceAnswer(problem);
+    boolean run = true;
+    while (run) {
+      Scanner input = new Scanner(System.in);
+      System.out.println("\nEnter your problem here or \"quit\": ");
+      String expression = input.nextLine();
+      expression = expression.toLowerCase();
+      if (!(expression.equals("quit"))) {
+        // basically this is saying that if a user DID NOT write 'quit' then we want to do something //
+        // code goes here //
+        System.out.println(produceAnswer(expression));
+      }
+      // else - user does not want to continue //
+      else {
+        System.out.println("BYE :)");
+        run = false;
+      }
+    }
   }
 
-public static String produceAnswer(String input){
-  String fraction1 = input.substring(0, input.indexOf(' '));
-  System.out.println("Frac 1:"  +fraction1);
-  String operator = input.substring(fraction1.length() + 1, fraction1.length() + 2);
-  System.out.println("Op:" +operator);
-  String fraction2 = input.substring(fraction1.length() + 3);
-  System.out.println("Frac 2:" +fraction2);
-  //return (fraction2);
-  String Whole = findWhole(fraction2);
-  String Num = findNum(fraction2);
-  String Denom = findDenom(fraction2);
-  String checkpoint2 = "Whole:" + Whole + "numerator:" + Num + "denominator:" +Denom;
-  return checkpoint2;
- }
- public static String findWhole(String str){
-   if (str.contains("_")){
-     return str.substring(0, str.indexOf('_'));
-   }else if (str.contains("/")){
-     return "0";
-  }else return str;
- }
-  public static String findNum (String str){
+
+ public static String produceAnswer(String input){
+   String fraction1 = input.substring(0, input.indexOf(' '));
+   System.out.println("Frac 1: "  +fraction1);
+   String operator = input.substring(fraction1.length() + 1, fraction1.length() + 2);
+   System.out.println("Op: " +operator);
+   String fraction2 = input.substring(fraction1.length() + 3);
+   System.out.println("Frac 2: " +fraction2);
+   //return (fraction2);
+   String Whole = findWhole(fraction2);
+   String Num = findNum(fraction2);
+   String Denom = findDenom(fraction2);
+  String checkpoint2 = "Whole: " + Whole + " Numerator: " + Num + "  Denominator: " + Denom;
+   return checkpoint2;
+  }//end produceAnswer
+
+  public static String findWhole(String str){
     if (str.contains("_")){
-      return str.substring(str.indexOf('_')+ 1, str.indexOf('/'));
+      return str.substring(0, str.indexOf('_'));
     }else if (str.contains("/")){
-      return str.substring(0,str.indexOf('/'));
-    }else{
-      return "0";
-    }
+      return "findWhole";
+   }else return str;
   }
-  public static String findDenom(String str){
-    if (str.contains("/")){
-      return str.substring(str.indexOf("/")+1);
-    }else{
-      return "1";
-    }
-  }
+  public static String findNum (String str){
+     if (str.contains("_")){
+       return str.substring(str.indexOf('_')+ 1, str.indexOf('/'));
+     }else if (str.contains("/")){
+       return str.substring(0,str.indexOf('/'));
+     }else{
+       return "findNum";
+     }
+   }
+   public static String findDenom(String str){
+     if (str.contains("/")){
+       return str.substring(str.indexOf("/")+1);
+     }else{
+       return "findDenom";
+     }
+   }
 }
